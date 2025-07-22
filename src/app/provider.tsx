a0487ecc72b0da2e9a37bfeb868b2e607e6c02d7
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-07-22 17:13:18 -0600

    Configured DevTools

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -1,10 +1,14 @@
 import { type PropsWithChildren } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { queryClient } from "@/libs/tanstack";
 
 export const AppProvider = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
