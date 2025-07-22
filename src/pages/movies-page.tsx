136034c7dbcb1b3a46a90600ee2a71001d91e659
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-07-21 18:07:40 -0600

    Implement hooks in pages

--- src/pages/movies-page.tsx
+++ src/pages/movies-page.tsx
@@ -1,29 +1,33 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { Link } from "react-router";
+import { Loading } from "@/components/core";
 import { MovieCard } from "@/features/movies/components";
-import type { MoviePreview } from "@/features/movies/types/movie-preview";
 import { PageHeader } from "@/components/layout";
 import { getMovies } from "@/features/movies/api/axios/getMovies";
 
 export const MoviesPage = () => {
-  const [movies, setMovies] = useState<MoviePreview[]>([]);
+  const { isPending, isError, data } = useQuery({
+    queryKey: ["movies"],
+    queryFn: ({ signal }) => getMovies({ signal }),
+  });
 
-  useEffect(() => {
-    const fetchMovies = async () => {
-      const data = await getMovies();
-      setMovies(data);
-    };
+  if (isPending) return <Loading sizeStyles="size-10" className="mx-auto" />;
 
-    fetchMovies();
-  }, []);
+  if (isError) {
+    return (
+      <div className="flex min-h-[200px] items-center justify-center">
+        <p className="text-red-500">Error loading movie. Please try again.</p>
+      </div>
+    );
+  }
 
   return (
     <div>
       <PageHeader title="Explore our movie collection" />
       <br />
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {movies.map((movie) => (
+        {data.map((movie) => (
           <li key={movie.id}>
             <Link to={`/movies/${movie.id}`}>
               <MovieCard title={movie.title} imageUrl={movie.thumbnail_url} />
