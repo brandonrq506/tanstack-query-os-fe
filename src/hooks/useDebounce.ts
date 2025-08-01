dbd1faac5fcf301bed18fac17a8d9e648308fa8a
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-07-31 23:49:58 -0600

    [9] Implement useDebounce

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+const DEBOUNCE_DELAY = 150;
+
+export function useDebounce<T>(value: T, delay: number = DEBOUNCE_DELAY): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const id = setTimeout(() => setDebouncedValue(value), delay);
+    return () => clearTimeout(id);
+  }, [value, delay]);
+
+  return debouncedValue;
+}
