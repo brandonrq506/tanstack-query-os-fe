8e12acd8d8a9c9b931f22b4221f2f6721856b7ce
brandonrq506 <91104615+brandonrq506@users.noreply.github.com>
2025-07-31 22:03:12 -0600

    [8] modal component (#4)

--- src/components/layout/main/sidebar/sidebar.tsx
+++ src/components/layout/main/sidebar/sidebar.tsx
@@ -60,7 +60,7 @@ export const Sidebar = ({ isOpen, onClose }: Props) => {
         </div>
       </Dialog>
 
-      <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
+      <div className="hidden lg:fixed lg:inset-y-0 lg:flex lg:w-72 lg:flex-col">
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
           <div className="flex h-16 shrink-0 items-center">
