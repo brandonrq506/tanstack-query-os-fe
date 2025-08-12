0da92cb9a6511dc27206c02a5a8a8af0a27c59f4
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-08-12 14:30:27 -0600

    [11] Created a new Carousel component

--- src/components/core/index.ts
+++ src/components/core/index.ts
@@ -1,3 +1,4 @@
+export { Carousel } from "./carousel/carousel";
 export { LinkButton } from "./button/link-button";
 export { Loading } from "./loading/loading";
 export { Modal } from "./modal/modal";
