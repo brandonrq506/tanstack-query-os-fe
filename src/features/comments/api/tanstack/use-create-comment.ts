ead5ad6def25707f88d67a090a2f1401f6748f17
Brandon Ramirez <bramirezq975@ulacit.ed.cr>
2025-09-02 17:31:01 -0600

    Open Space - Session 7

--- src/features/comments/api/tanstack/use-create-comment.ts
+++ src/features/comments/api/tanstack/use-create-comment.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-import { commentKeyFactory } from "../comment-key-factory";
+import type { CommentModel } from "../../types/comment-model";
+import { commentsByMovieOptions } from "../query-options/comments-by-movie-options";
 import { createComment } from "../axios/create-comment";
 
 export const useCreateComment = () => {
@@ -8,8 +9,48 @@ export const useCreateComment = () => {
 
   return useMutation({
     mutationFn: createComment,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: commentKeyFactory.all });
+    onMutate: async ({ movieId, body }) => {
+      // Do not invalidate the following query just yet!
+      await queryClient.cancelQueries(commentsByMovieOptions(movieId));
+
+      // This is just a helper
+      const queryKey = commentsByMovieOptions(movieId).queryKey;
+
+      // Obtain the previous (current) comments
+      const prevComments = queryClient.getQueryData(queryKey);
+
+      // New comment
+      const newComment: CommentModel = {
+        id: Date.now(),
+        body,
+        movie_id: movieId,
+        author_color: "blue",
+        author_name: "Temporary Otter",
+        created_at: new Date().toISOString(),
+        updated_at: new Date().toISOString(),
+      };
+
+      // Update cache optimistically
+      queryClient.setQueryData(queryKey, (oldComments) => {
+        if (!oldComments) return [newComment];
+        return [newComment, ...oldComments];
+      });
+
+      return { prevComments };
+    },
+
+    onError: (_, { movieId }, context) => {
+      // Rollback to the previous comments
+      if (context?.prevComments) {
+        queryClient.setQueryData(
+          commentsByMovieOptions(movieId).queryKey,
+          context.prevComments,
+        );
+      }
+    },
+
+    onSettled: (_, __, { movieId }) => {
+      queryClient.invalidateQueries(commentsByMovieOptions(movieId));
     },
   });
 };
--- src/features/comments/api/tanstack/use-delete-comment.ts
+++ src/features/comments/api/tanstack/use-delete-comment.ts
@@ -1,15 +1,47 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useParams } from "react-router";
 
-import { commentKeyFactory } from "../comment-key-factory";
+import { commentsByMovieOptions } from "../query-options/comments-by-movie-options";
 import { deleteComment } from "../axios/delete-comment";
 
 export const useDeleteComment = () => {
   const queryClient = useQueryClient();
+  const { movieId } = useParams();
+
+  if (!movieId) throw new Error("movieId is required");
+
+  const movie_id = Number(movieId);
 
   return useMutation({
     mutationFn: deleteComment,
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: commentKeyFactory.all });
+    onMutate: async (commentId) => {
+      // Cancel any outgoing refetches
+      await queryClient.cancelQueries(commentsByMovieOptions(movie_id));
+
+      const queryKey = commentsByMovieOptions(movie_id).queryKey;
+
+      // Snapshot the previous value
+      const prevComments = queryClient.getQueryData(queryKey);
+
+      // Optimistically remove the comment
+      queryClient.setQueryData(queryKey, (old) => {
+        if (!old) return old;
+        return old.filter((c) => c.id !== commentId);
+      });
+
+      return { prevComments };
+    },
+    onError: (_, __, context) => {
+      // If the mutation fails, use the context returned from onMutate to roll back
+      if (context?.prevComments) {
+        queryClient.setQueryData(
+          commentsByMovieOptions(movie_id).queryKey,
+          context.prevComments,
+        );
+      }
+    },
+    onSettled: () => {
+      queryClient.invalidateQueries(commentsByMovieOptions(movie_id));
     },
   });
 };
